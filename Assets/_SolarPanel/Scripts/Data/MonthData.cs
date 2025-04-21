037f657da511e72ac86a376d262f54b3dcaab0a0
IcamyS <105745467+icamysss@users.noreply.github.com>
2025-04-21 11:28:17 +0300

    + City Data SO

--- Assets/_SolarPanel/Scripts/Data/MonthData.cs
+++ Assets/_SolarPanel/Scripts/Data/MonthData.cs
@@ -2,15 +2,20 @@ namespace _SolarPanel.Scripts.Data
 {
     [System.Serializable]
     public class MonthData {
-        public string MonthName; // Название месяца (например, "Январь")
+        public MonthType MonthName; // Название месяца
         public float Insolation;  // Солнечная инсоляция (кВт·ч/м²)
         public int OptimalTilt;   // Оптимальный угол наклона (°)
 
         // Конструктор для удобного заполнения данных
-        public MonthData(string monthName, float insolation, int optimalTilt) {
+        public MonthData(MonthType monthName, float insolation, int optimalTilt) {
             MonthName = monthName;
             Insolation = insolation;
             OptimalTilt = optimalTilt;
         }
+    }
+
+    public enum MonthType
+    {
+        Январь, Февраль, Март, Апрель, Май, Июнь, Июль, Август, Сентябрь, Октябрь, Ноябрь, Декабрь
     }
 }
--- Assets/_SolarPanel/Scripts/Data/SO/CityDataSO.cs
+++ Assets/_SolarPanel/Scripts/Data/SO/CityDataSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,6 +6,33 @@ namespace _SolarPanel.Scripts.Data.SO
 {
     [CreateAssetMenu(fileName = "CityDataSO", menuName = "SolarApp/CityData")]
     public class CityDataSO : ScriptableObject {
-        public List<City> Cities = new List<City>();
+        public List<City> Cities = new ();
+
+        public float GetAverageAnnualSolarInsolationForTown(string townName)
+        {
+           
+            var town = Cities.Find(x => x.Name == townName);
+            if (town == null) throw new NullReferenceException("Такой город не найден!");
+            var insolation = 0f;
+           
+            foreach (var month in town.MonthlyData)
+            {
+                insolation += month.Insolation;
+            }
+            return insolation / town.MonthlyData.Count; ;
+        }
+
+        public float GetAverageOptimalAngleForTown(string townName)
+        {
+            var town = Cities.Find(x => x.Name == townName);
+            if (town == null) throw new NullReferenceException("Такой город не найден!");
+            var angle = 0f;
+          
+            foreach (var month in town.MonthlyData)
+            {
+                angle += month.OptimalTilt;
+            }
+            return angle / town.MonthlyData.Count;
+        }
     }
 }
