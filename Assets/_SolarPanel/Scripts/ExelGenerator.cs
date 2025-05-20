3be3053cc0f05d13ee4adb8c521b2fed0fa79da2
IcamyS <105745467+icamysss@users.noreply.github.com>
2025-05-20 16:34:19 +0300

    + exel

---
+++ Assets/_SolarPanel/Scripts/ExelGenerator.cs
@@ -0,0 +1,31 @@
+namespace _SolarPanel.Scripts
+{
+    using UnityEngine;
+    using System.IO;
+    // using NPOI.HSSF.UserModel; // Для XLS
+    // using NPOI.SS.UserModel;
+
+    public class ExcelGenerator : MonoBehaviour
+    {
+        // void Start()
+        // {
+        //     // Создаем новую книгу Excel (XLS)
+        //     IWorkbook workbook = new HSSFWorkbook();
+        //     ISheet sheet = workbook.CreateSheet("Sheet1");
+        //
+        //     // Заполняем данные
+        //     IRow row = sheet.CreateRow(0);
+        //     row.CreateCell(0).SetCellValue("Hello");
+        //     row.CreateCell(1).SetCellValue("World!");
+        //
+        //     // Сохраняем файл
+        //     string filePath = Path.Combine(Application.persistentDataPath, "test.xls");
+        //     using (FileStream fs = new FileStream(filePath, FileMode.Create))
+        //     {
+        //         workbook.Write(fs);
+        //     }
+        //
+        //     Debug.Log($"Excel файл создан: {filePath}");
+        }
+    }
+}
